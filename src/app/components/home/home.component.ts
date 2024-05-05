3fae287920a6f9b2dbbef4d1cd179059b60cd5e4
CaduLopes87 <edu.lopesbezerra@gmail.com>
2024-05-04 21:44:08 -0300

    FEAT: Create the catalog component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { CatalogComponent } from "../catalog/catalog.component";
+import { PresentationComponent } from '../presentation/presentation.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, CatalogComponent, PresentationComponent]
 })
 export class HomeComponent {
 
