67232712768af16af79599f8f38ee203164b2e77
CaduLopes87 <edu.lopesbezerra@gmail.com>
2024-05-05 23:12:28 -0300

    FEAT: Create the common questions component & Devlop the expansive button

---
+++ src/app/components/common-questions/common-questions.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ToggleButtonInterface } from '../../interfaces/toggle-button-content';
+
+@Component({
+  selector: 'app-common-questions',
+  standalone: true,
+  imports: [],
+  templateUrl: './common-questions.component.html',
+  styleUrl: './common-questions.component.scss',
+})
+export class CommonQuestionsComponent {
+  isVisible: ToggleButtonInterface = {};
+
+  toggleContent(id: number) {
+    if (this.isVisible[id] === undefined) {
+      this.isVisible[id] = false;
+      this.isVisible[id] = !this.isVisible[id];
+    } else {
+      this.isVisible[id] = !this.isVisible[id];
+    }
+  }
+}
