3fdac7865350d2747cc97d518ae34791f6641477
CaduLopes87 <edu.lopesbezerra@gmail.com>
2024-05-10 21:35:18 -0300

    UPDATE: Changes the home components path into the home component directory

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { CatalogComponent } from '../catalog/catalog.component';
-import { PresentationComponent } from '../presentation/presentation.component';
-import { CommonQuestionsComponent } from '../common-questions/common-questions.component';
+import { HeaderComponent } from './header/header.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CommonQuestionsComponent } from './common-questions/common-questions.component';
 
 @Component({
   selector: 'app-home',
